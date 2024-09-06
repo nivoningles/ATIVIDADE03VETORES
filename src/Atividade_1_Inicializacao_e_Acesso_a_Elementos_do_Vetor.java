275178a3dce04da43370ea88640541f59a7fd3a1
nivoningles <contadonivon@gmail.com>
2024-09-05 21:12:34 -0300

    Atividades 3

---
+++ src/Atividade_1_Inicializacao_e_Acesso_a_Elementos_do_Vetor.java
@@ -0,0 +1,13 @@
+public class Atividade_1_Inicializacao_e_Acesso_a_Elementos_do_Vetor {
+    public static void main(String[] args) {
+        int[] numero = new int[5];
+
+        numero[0] = 10;
+        numero[1] = 20;
+        numero[2] = 30;
+        numero[3] = 40;
+        numero[4] = 50;
+
+        System.out.println("O terceiro elemento é "+numero[2]);
+    }
+}
