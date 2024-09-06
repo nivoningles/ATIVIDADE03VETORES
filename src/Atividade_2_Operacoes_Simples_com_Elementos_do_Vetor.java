e153d35038514107457889401059a4672027f676
nivoningles <contadonivon@gmail.com>
2024-09-05 21:19:29 -0300

    Atividades 3

---
+++ src/Atividade_2_Operacoes_Simples_com_Elementos_do_Vetor.java
@@ -0,0 +1,14 @@
+public class Atividade_2_Operacoes_Simples_com_Elementos_do_Vetor {
+    public static void main(String[] args) {
+        int[] numero = new int[4];
+
+        numero[0]=3;
+        numero[1]=6;
+        numero[2]=9;
+        numero[3]=12;
+
+        int soma = numero[0]+numero[3];
+
+        System.out.println("A soma do primeiro número e o ultimo número é: " + soma);
+    }
+}
